c8f8e3f67d6f0a215e7e6a4aafaf37492138140d
Pasha <pashahrytsyna@gmail.com>
2021-11-17 18:10:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar/NavBar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="topnav">
+        <a href='#'>Movies App</a>
+        <a className="active" href="#home">
+          Home
+        </a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <div className="search-container">
+          <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search" />
+            <button type="submit">
+              <i className="fa fa-search"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
